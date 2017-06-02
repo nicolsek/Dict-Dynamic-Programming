48492fc9e18f3a1e72510c460b30a980dee8d96e
GeekishGirl <tusek.nicole@gmail.com>
2017-06-01 20:40:34 -0700

    Created functions for description of lines and dictionary checking.

--- dynProg.cpp
+++ dynProg.cpp
@@ -1,14 +1,33 @@
 #include <iostream>
 #include <stdio.h>
 
+// dict ... Check if the passed word is valid in the dictionary.
+bool dict(const char *word) {
+    bool isWord = false;
+
+    return isWord;
+}
+
+// split ... Is it possible to add spaces to x1 ... xn to create a string of English words.
+bool split(const char *line) {
+    bool canSplit = false;
+
+    return canSplit;
+}
+
 // handleInput ... Handle the data given from standard input.
 void handleInput() {
     //First line is C -- The amount of lines there are.
     unsigned int C;
     std::cin >> C;
 
+    //Creating the loop that reads all of the lines.
+    for (int line = 0; line < C; line++) {
+    
+    }
+
     //Debug information.
-    std::cout << "There are " << C << " lines." << std::endl;
+    //std::cout << "There are " << C << " lines." << std::endl;
 }
 
 // main ... The main function.
